2c274e93a54a238506f6c0ce3c5b9b67601c0c2d
Nick Schimek <schimek@gmail.com>
2023-05-25 12:56:46 -0500

    image tests

--- service/image_test.go
+++ service/image_test.go
@@ -1,41 +1,93 @@
 package service
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"testing"
 
 	"github.com/nschimek/nice-fixture-feeder/core"
 	"github.com/stretchr/testify/suite"
 )
 
 const (
+	testFileName = "test.txt"
+	testBucket = "test-bucket"
+	testKeyFormat = "/test/%s"
 	testData = "dummy test data"
+	testFinalKeyName = "/test/test.txt"
 )
 
 type ImageServiceTestSuite struct {
 	suite.Suite
-	mockHttp *httptest.Server
 	mockS3 *core.MockS3Client
+	is *imageService
+}
+
+func TestLeagueRepositoryTestSuite(t *testing.T) {
+	suite.Run(t, new(ImageServiceTestSuite))
 }
 
 func (suite *ImageServiceTestSuite) SetupTest() {
-	suite.mockHttp = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, testData)
-	}))
-	defer suite.mockHttp.Close()
 	suite.mockS3 = &core.MockS3Client{}
+	suite.is = &imageService{s3: suite.mockS3}
 }
 
 func (suite *ImageServiceTestSuite) TestSuccessful() {
-	url, bucket, keyFormat := "http://wwww.test.com/test.txt", "test-bucket", "/test/%s"
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, testData)
+	}))
+	defer svr.Close()
 
-	suite.mockS3.On("Exists", bucket, "/test/test-bucket/test.txt").Return(false, nil)
-	suite.mockS3.On("Upload", []byte(testData), bucket, "/test/test-bucket/test.txt").Return(nil)
+	suite.mockS3.On("Exists", testBucket, testFinalKeyName).Return(false, nil)
+	suite.mockS3.On("Upload", []byte(testData), testBucket, testFinalKeyName).Return(nil)
 
-	is := &imageService{s3: suite.mockS3}
-
-	r := is.TransferURL(url, bucket, keyFormat)
+	r := suite.is.TransferURL(svr.URL + "/" + testFileName, testBucket, testKeyFormat)
 
 	suite.True(r)
+}
+
+func (suite *ImageServiceTestSuite) TestExistsError() {
+	suite.mockS3.On("Exists", testBucket, testFinalKeyName).Return(false, errors.New("test"))
+
+	// url does not matter for this test, as we never get there
+	r := suite.is.TransferURL(testFileName, testBucket, testKeyFormat)
+
+	suite.False(r)
+}
+
+func (suite *ImageServiceTestSuite) TestDownloadError() {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Test Error", http.StatusBadRequest)
+	}))
+	defer svr.Close()
+
+	suite.mockS3.On("Exists", testBucket, testFinalKeyName).Return(false, nil)
+
+	r := suite.is.TransferURL(svr.URL + "/" + testFileName, testBucket, testKeyFormat)
+
+	suite.False(r)
+}
+
+func (suite *ImageServiceTestSuite) TestUploadError() {
+	svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, testData)
+	}))
+	defer svr.Close()
+
+	suite.mockS3.On("Exists", testBucket, testFinalKeyName).Return(false, nil)
+	suite.mockS3.On("Upload", []byte(testData), testBucket, testFinalKeyName).Return(errors.New("test"))
+
+	r := suite.is.TransferURL(svr.URL + "/" + testFileName, testBucket, testKeyFormat)
+	suite.False(r)
+}
+
+func (suite *ImageServiceTestSuite) TestImageExists() {
+	suite.mockS3.On("Exists", testBucket, testFinalKeyName).Return(true, nil)
+
+	// url does not matter for this test, as we never get there
+	r := suite.is.TransferURL(testFileName, testBucket, testKeyFormat)
+
+	suite.False(r)
 }
