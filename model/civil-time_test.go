3261d49a20dc273a53a16857afa95371618d20e6
Nick Schimek <schimek@gmail.com>
2023-05-29 20:24:51 -0500

    more civil time tests

--- model/civil-time_test.go
+++ model/civil-time_test.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -18,4 +19,32 @@ func TestUnmarshalValid(t *testing.T) {
 	a.UnmarshalJSON([]byte(date))
 
 	assert.Equal(t, e, a)
+}
+
+func TestUnmarshalNull(t *testing.T) {
+	e := new(CivilTime)
+	*e = CivilTime(time.Time{})
+
+	a := new(CivilTime)
+	a.UnmarshalJSON([]byte(""))
+
+	assert.Equal(t, e, a)
+}
+
+func TestUnmarshalInvalid(t *testing.T) {
+	a := new(CivilTime)
+	assert.Error(t, a.UnmarshalJSON([]byte("20220529")))
+}
+
+func TestMarshal(t *testing.T) {
+	date := "2023-05-26"
+	dateTime, _ := time.Parse("2006-01-02", date)
+
+	a := new(CivilTime)
+	*a = CivilTime(dateTime)
+	ab, _ := a.MarshalJSON() // actual bytes
+
+	eb := []byte(fmt.Sprintf("\"%s\"", date)) // expected bytes
+	
+	assert.Equal(t, eb, ab)
 }
