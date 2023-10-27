fe4ddb7d127d463fe75fc8cca48e9a41a0ce389b
Nick Schimek <schimek@gmail.com>
2023-10-26 21:31:08 -0500

    GTE!

--- repository/fixture.go
+++ repository/fixture.go
@@ -5,7 +5,7 @@ import (
 	"github.com/nschimek/nice-fixture-feeder/model"
 )
 
-const futureFixtureSQL = "league_id = ? AND season = ? and (team_home_id = ? or team_away_id = ?) and fixture_id > ?"
+const futureFixtureSQL = "league_id = ? AND season = ? and (team_home_id = ? or team_away_id = ?) and fixture_id >= ?"
 
 type Fixture interface {
 	UpsertRepository[model.Fixture]
@@ -24,7 +24,7 @@ func NewFixture(db core.Database) *fixture {
 	}
 }
 
-// Find all fixtures with an ID greater than Min ID with the same Season, League, and Team ID (home or away) using the TLS
+// Find all fixtures with an ID GTE than Min ID with the same Season, League, and Team ID (home or away) using the TLS
 func (r *fixture) GetFutureFixturesByTLS(tlsId model.TeamLeagueSeasonId, minId int) ([]model.Fixture, error) {
 	var fixtures []model.Fixture
 	if err := r.db.Where(&fixtures, futureFixtureSQL, tlsId.LeagueId, 
