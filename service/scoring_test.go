fdb8b55fe20e0a110ef803884fafca0d02ca38ea
Nick Schimek <schimek@gmail.com>
2023-11-13 21:49:17 -0600

    continue scoring service tests

--- service/scoring_test.go
+++ service/scoring_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/nschimek/nice-fixture-feeder/model"
@@ -49,6 +50,7 @@ func (s *scoringTestSuite) SetupTest() {
 	s.fixtures = []model.Fixture{
 		{Fixture: model.FixtureFixture{Id: 100}},
 		{Fixture: model.FixtureFixture{Id: 101}},
+		{Fixture: model.FixtureFixture{Id: 102}},
 	}
 	s.fixtureScores = []model.FixtureScore{
 		{FixtureId: 100, ScoreId: 1, Value: 67},
@@ -89,15 +91,72 @@ func (s *scoringTestSuite) TestScore() {
 	s.scoring.SetFixtures(map[int]model.Fixture{
 		100: s.fixtures[0],
 		101: s.fixtures[1],
+		102: s.fixtures[2],
 	})
 
 	s.mockScorePS.EXPECT().CanScore(&s.fixtures[0]).Return(true)
 	s.mockScorePS.EXPECT().CanScore(&s.fixtures[1]).Return(false)
+	s.mockScorePS.EXPECT().CanScore(&s.fixtures[2]).Return(true)
 	s.mockScorePS.EXPECT().Score(&s.fixtures[0]).Return(&s.fixtureScores[0], nil)
+	s.mockScorePS.EXPECT().Score(&s.fixtures[2]).Return(nil, errors.New("test")) // coverage
 	s.mockFixtureScoreRepo.EXPECT().Upsert(s.fixtureScores[0:1]).Return(s.fixtureScores[0:1], nil)
 	
 	s.scoring.Score()
 	
 	s.mockScorePS.AssertExpectations(s.T())
 	s.mockFixtureScoreRepo.AssertCalled(s.T(), "Upsert", s.fixtureScores[0:1])
+}
+
+func (s *scoringTestSuite) TestGetStatsTeam() {
+	fixture := model.Fixture{
+		League: model.FixtureLeague{Id: 10, Season: 2022, Round: 5}, 
+		Teams: model.FixtureTeams{Home: model.FixtureTeam{Id: 1}}, 
+		Fixture: model.FixtureFixture{Id: 100, Status: model.FixtureStatusId{Id: "FT"}},
+	}
+	stats := model.TeamStats{Id: model.TeamStatsId{FixtureId: 100}}
+
+	s.mockStatusService.EXPECT().IsScheduled("FT").Return(false)
+	s.mockStatsService.EXPECT().GetById(model.TeamStatsId{
+		TeamId: 1, LeagueId: 10, Season: 2022, NextFixtureId: 100}).Return(&stats, nil)
+
+	res, err := s.scoring.getStatsTeam(&fixture, true)
+
+	s.NoError(err)
+	s.Equal(&stats, res)
+}
+
+func (s *scoringTestSuite) TestGetStatsTeamScheduled() {
+	fixture := model.Fixture{
+		League: model.FixtureLeague{Id: 10, Season: 2022, Round: 5}, 
+		Teams: model.FixtureTeams{Home: model.FixtureTeam{Id: 1}}, 
+		Fixture: model.FixtureFixture{Id: 100, Status: model.FixtureStatusId{Id: "SC"}},
+	}
+	stats := model.TeamStats{Id: model.TeamStatsId{FixtureId: 100}}
+
+	s.mockStatusService.EXPECT().IsScheduled("SC").Return(true)
+	s.mockStatsService.EXPECT().GetByIdWithTLS(model.TeamStatsId{
+		TeamId: 1, LeagueId: 10, Season: 2022, NextFixtureId: 100}, false).Return(&stats, nil)
+
+	res, err := s.scoring.getStatsTeam(&fixture, true)
+
+	s.NoError(err)
+	s.Equal(&stats, res)
+}
+
+func (s *scoringTestSuite) TestGetStatsTeamEarlyRound() {
+	fixture := model.Fixture{
+		League: model.FixtureLeague{Id: 10, Season: 2022, Round: 1}, 
+		Teams: model.FixtureTeams{Home: model.FixtureTeam{Id: 1}}, 
+		Fixture: model.FixtureFixture{Id: 100, Status: model.FixtureStatusId{Id: "FT"}},
+	}
+	stats := model.TeamStats{Id: model.TeamStatsId{FixtureId: 100}}
+
+	s.mockStatusService.EXPECT().IsScheduled("SC").Return(false)
+	s.mockStatsService.EXPECT().GetByIdWithTLS(model.TeamStatsId{
+		TeamId: 1, LeagueId: 10, Season: 2021, NextFixtureId: 100}, true).Return(&stats, nil)
+
+	res, err := s.scoring.getStatsTeam(&fixture, true)
+
+	s.NoError(err)
+	s.Equal(&stats, res)
 }
