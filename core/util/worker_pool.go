db586a056cd9f42a3c256752602b2678b62cd06e
Nick Schimek <schimek@gmail.com>
2024-01-17 21:52:13 -0600

    error handling for worker pool?

--- core/util/worker_pool.go
+++ core/util/worker_pool.go
@@ -5,27 +5,46 @@ import (
 )
 
 type WorkerPool struct {
-	size int
-	wg sync.WaitGroup
+	workers int
+	wg      sync.WaitGroup
+	errc    chan error
+	errors  []error
 }
 
-func NewWorkerPool(size int) *WorkerPool {
-	return &WorkerPool{size: size}
+// NewWorkerPool creates a new worker pool with the specified number of workers.
+func NewWorkerPool(workers int) *WorkerPool {
+	return &WorkerPool{workers: workers, errc: make(chan error), errors: make([]error, workers)}
 }
 
-func (pool *WorkerPool) Go(fn func(worker int)) {
-	pool.wg.Add(pool.size)
-	for i := 0; i < pool.size; i++ {
+func (pool *WorkerPool) Go(fn func(worker int) error) {
+	pool.wg.Add(pool.workers)
+	for i := 0; i < pool.workers; i++ {
 		go func(worker int) {
-			fn(worker)
+			if err := fn(worker); err != nil {
+				pool.errc <- err
+			}
 			pool.wg.Done()
 		}(i)
 	}
+	go func() {
+		for err := range pool.errc {
+			pool.errors = append(pool.errors, err)
+		}
+	}()
 }
 
 func (pool *WorkerPool) Wait(fn func()) {
 	go func() {
 		pool.wg.Wait()
+		close(pool.errc)
 		fn()
 	}()
+}
+
+func (pool *WorkerPool) HasErrors() bool {
+	return len(pool.errors) > 0
+}
+
+func (pool *WorkerPool) Errors() []error {
+	return pool.errors
 }
